6a3be5dc3b23d1db6f781452aad7d26b60c1e775
Tanner Townsend <rsmcwow@gmail.com>
2020-03-31 21:06:02 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,7 +84,6 @@ def adding_matz
         }
      }
 
-programmer_hash[:yukihiro_matsumoto] = :known_for, :languages
 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
 programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
 
